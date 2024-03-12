0389d76e0966916ea33ff3752184a5d244779477
bjmickens <128921000+bjmickens@users.noreply.github.com>
2024-03-11 20:34:39 -0500

    Add files via upload

---
+++ part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class FromWhereToWhere {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+        System.out.print("Where to? ");
+        int to = Integer.valueOf(scanner.nextLine());
+
+        System.out.print("Where from? ");
+        int from = Integer.valueOf(scanner.nextLine());
+
+        for (int i=from; i <= to; i++){
+            if (to >= from){
+                System.out.println(i);
+            }
+        }
+    }
+}
